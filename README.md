2bc0dd00694ae12be502e1d52ecf3bcb98927117
Peer Naupold <greenion@gmx.de>
2020-05-16 19:07:29 +0200

    added readme.md and .travis.yml

---
+++ README.md
@@ -0,0 +1,2 @@
+
+[![Build Status](https://travis-ci.org/poldi2018/poldis_cookbook.svg?branch=master)](https://travis-ci.org/poldi2018/poldis_cookbook)
--- tests/test_views.py
+++ tests/test_views.py
@@ -1,6 +1,8 @@
 from flask_testing import TestCase
 import unittest
 from app import app
+#from flask import Flask
+#from flask_testing import LiveServerTestCase
 
 class test_poldi(unittest.TestCase):
     """ Checking working test kit """
@@ -9,12 +11,13 @@ class test_poldi(unittest.TestCase):
 
 class test_views(unittest.TestCase):
     def setUp(self):
-        self.app = app.test_client()
+        self.app = app.test_client(use_cookies=True)
+        #client = self.app.test_client(use_cookie=True)
 
-    def test_index_v1(self):
+    def test_index(self):
         response = self.app.get("/", content_type="html/text", follow_redirects=True)
         self.assertEqual(response.status_code, 200)
-        
+        #self.assert_template_used('index.html')
 
 
 if __name__ == '__main__':
